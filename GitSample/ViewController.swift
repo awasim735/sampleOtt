b09d2c77e048120b53a7547d4b19f7c9faa51a89
admin <admin@Waseem-Akram-Mac.local>
2018-09-27 12:02:01 +0530

    first commit

--- GitSample/ViewController.swift
+++ GitSample/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("Waseem Akram")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
